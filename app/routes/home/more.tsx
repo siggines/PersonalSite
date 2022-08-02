ca99d8aa667dbe27ec72f879d5add4dfe9d3f9fb
DanielJSadler <79659481+DanielJSadler@users.noreply.github.com>
2022-08-02 20:04:42 +0100

    rick

--- app/routes/home/more.tsx
+++ app/routes/home/more.tsx
@@ -1,15 +1,18 @@
+import { Link, Outlet } from "@remix-run/react";
+
 export default function HomeMore() {
   return (
-    <div>
+    <Link to="/home">
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ/?autoplay=1"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-    </div>
+      <Outlet />
+    </Link>
   );
 }
