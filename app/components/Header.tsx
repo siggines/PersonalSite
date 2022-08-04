7fc0777562bed53a4c229f5c5c6df2c66ea48ece
DanielJSadler <79659481+DanielJSadler@users.noreply.github.com>
2022-08-04 12:20:58 +0100

    mixesStart

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,15 +3,31 @@ import { NavLink } from "@remix-run/react";
 export function Header() {
   return (
     <header>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">
+            <span className="material-symbols-outlined">Home</span>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/map">
+            <span className="material-symbols-outlined">map</span>Map
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/map">Map</NavLink>
+          <NavLink to="/mixes">
+            <span className="material-symbols-outlined">music_note</span>Mixes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">
+            <span className="material-symbols-outlined">chat</span>Contact
+          </NavLink>
         </li>
       </ul>
     </header>
