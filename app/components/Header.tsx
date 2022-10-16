69c3c904420b9c687f24cb190e114f4e0777b3d1
DanielSadler <112938632+DanielSadler@users.noreply.github.com>
2022-10-16 16:45:52 +0100

    tailwind

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,29 +2,29 @@ import { NavLink } from "@remix-run/react";
 
 export function Header() {
   return (
-    <header>
+    <header className="bg-slate-600 h-14 text-white text-sm">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
       />
-      <ul>
-        <li>
+      <ul className="list-none h-full flex flex-row items-center p-2 justify-end">
+        <li className=" mr-2 rounded-md hover:bg-white hover:bg-opacity-25 p-1 duration-700">
           <NavLink to="/">
             <span className="material-symbols-outlined">Home</span>
             Home
           </NavLink>
         </li>
-        <li>
+        <li className=" mr-2 rounded-md hover:bg-white hover:bg-opacity-25 p-1 duration-700">
           <NavLink to="/map">
             <span className="material-symbols-outlined">map</span>Map
           </NavLink>
         </li>
-        <li>
+        <li className=" mr-2 rounded-md hover:bg-white hover:bg-opacity-25 p-1 duration-700">
           <NavLink to="/mixes">
             <span className="material-symbols-outlined">music_note</span>Mixes
           </NavLink>
         </li>
-        <li>
+        <li className=" mr-2 rounded-md hover:bg-white hover:bg-opacity-25 p-1 duration-700">
           <NavLink to="/contact">
             <span className="material-symbols-outlined">chat</span>Contact
           </NavLink>
